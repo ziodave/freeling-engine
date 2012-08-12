54e4065fefe81cf37358f9e7858ae756f73fdb90
David Riccitelli <ziodave@gmail.com>
2012-08-12 22:04:39 +0300

    initial version of POS Tagger engine

--- src/main/java/io/insideout/wordlift/org/apache/stanbol/enhancer/engines/freeling/impl/FreelingPartOfSpeechTaggingEngine.java
+++ src/main/java/io/insideout/wordlift/org/apache/stanbol/enhancer/engines/freeling/impl/FreelingPartOfSpeechTaggingEngine.java
@@ -1,14 +1,16 @@
 package io.insideout.wordlift.org.apache.stanbol.enhancer.engines.freeling.impl;
 
+import static org.apache.stanbol.enhancer.servicesapi.rdf.Properties.DC_TYPE;
+import static org.apache.stanbol.enhancer.servicesapi.rdf.Properties.ENHANCER_CONFIDENCE;
 import static org.apache.stanbol.enhancer.servicesapi.rdf.Properties.ENHANCER_END;
 import static org.apache.stanbol.enhancer.servicesapi.rdf.Properties.ENHANCER_SELECTED_TEXT;
 import static org.apache.stanbol.enhancer.servicesapi.rdf.Properties.ENHANCER_START;
-import static org.apache.stanbol.enhancer.servicesapi.rdf.Properties.ENHANCER_CONFIDENCE;
 import io.insideout.wordlift.org.apache.stanbol.domain.Noun;
 import io.insideout.wordlift.org.apache.stanbol.services.StanbolService;
 
 import java.io.File;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
@@ -33,6 +35,14 @@ import org.apache.stanbol.enhancer.servicesapi.ServiceProperties;
 import org.apache.stanbol.enhancer.servicesapi.helper.ContentItemHelper;
 import org.apache.stanbol.enhancer.servicesapi.helper.EnhancementEngineHelper;
 import org.apache.stanbol.enhancer.servicesapi.impl.AbstractEnhancementEngine;
+import org.apache.stanbol.entityhub.servicesapi.Entityhub;
+import org.apache.stanbol.entityhub.servicesapi.EntityhubException;
+import org.apache.stanbol.entityhub.servicesapi.model.Reference;
+import org.apache.stanbol.entityhub.servicesapi.model.Representation;
+import org.apache.stanbol.entityhub.servicesapi.query.FieldQuery;
+import org.apache.stanbol.entityhub.servicesapi.query.QueryResultList;
+import org.apache.stanbol.entityhub.servicesapi.query.TextConstraint;
+import org.apache.stanbol.entityhub.servicesapi.site.Site;
 import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
@@ -57,7 +67,12 @@ public class FreelingPartOfSpeechTaggingEngine extends
     private final String configurationPath = "/usr/local/Cellar/freeling/HEAD/share/freeling/config";
     private final String configurationFilenameSuffix = ".cfg";
 
+    private String fieldName = "rdfs:label";
+    private String fieldType = "rdf:type";
+
     private PartOfSpeechTagging partOfSpeechTagging;
+    private Entityhub entityHub;
+    private Site site;
 
     /**
      * The default value for the Execution of this Engine. Currently set to
@@ -72,6 +87,13 @@ public class FreelingPartOfSpeechTaggingEngine extends
         logger.trace("The Freeling engine is being activated.");
 
         partOfSpeechTagging = new PartOfSpeechTagging();
+
+        entityHub = StanbolService.getEntityhub(context);
+        logger.trace("The Entity Hub has been bound [{}].", entityHub.getClass().toString());
+
+        site = StanbolService.getSite(context);
+        logger.trace("A site has been bound [{}].", site.getClass().toString());
+
     }
 
     @Deactivate
@@ -127,12 +149,38 @@ public class FreelingPartOfSpeechTaggingEngine extends
         ci.getLock().writeLock().lock();
         try {
             for (Noun noun : nouns) {
+
+                FieldQuery fieldQuery = site.getQueryFactory().createFieldQuery();
+                try {
+                    fieldQuery.addSelectedField(fieldName);
+                    fieldQuery.setLimit(20);// TODO make configurable
+                    fieldQuery.setConstraint(fieldName, new TextConstraint(noun.getWord(),
+                            languageTwoLetterCode));
+                    QueryResultList<Representation> representations = entityHub.find(fieldQuery);
+                    logger.trace("Found [{}] representation(s) for word [{}].", representations.size(),
+                        noun.getWord());
+                    for (Representation representation : representations) {
+                        Iterator<Reference> referencesIterator = representation.getReferences(fieldType);
+                        while (referencesIterator.hasNext()) {
+                            Reference reference = referencesIterator.next();
+                            logger.trace("Found a reference [word :: {}][reference :: {}].", noun.getWord(),
+                                reference.getReference());
+                        }
+
+                    }
+
+                } catch (EntityhubException e) {
+                    logger.error("The EntityHub [{}] raised an exception [{}]:\n{}",
+                        new Object[] {entityHub.getClass(), e.getClass(), e.getMessage()}, e);
+                }
+
                 UriRef textAnnotation = EnhancementEngineHelper.createTextEnhancement(ci, this);
                 g.add(new TripleImpl(textAnnotation, ENHANCER_SELECTED_TEXT, new PlainLiteralImpl(noun
                         .getWord(), language)));
                 // g.add(new TripleImpl(textAnnotation, ENHANCER_SELECTION_CONTEXT, new PlainLiteralImpl(
                 // occurrence.context, language)));
-                // g.add(new TripleImpl(textAnnotation, DC_TYPE, typeUri));
+                g.add(new TripleImpl(textAnnotation, DC_TYPE, new UriRef(
+                        "http://www.w3.org/2002/07/owl#Thing")));
                 g.add(new TripleImpl(textAnnotation, ENHANCER_CONFIDENCE, LiteralFactory.getInstance()
                         .createTypedLiteral(noun.getConfidence())));
                 // if (occurrence.start != null && occurrence.end != null) {
